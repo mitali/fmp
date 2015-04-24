73a63250d8dc536f3b3cffc410884ca264f4495b
Mitali <mitalidargani@gmail.com>
2015-04-24 22:05:55 +0100

    Successfully set up project env. OpenCV works in PyDev. Successssss

--- FilmAudioProgram/video/videoprac.py
+++ FilmAudioProgram/video/videoprac.py
@@ -1,19 +1,15 @@
 __author__ = 'Mitali'
 
-import numpy as np
-import sys
 import cv2
 
-sys.path.append('/usr/local/lib/python2.7/site-packages')
-
-cap = cv2.VideoCapture(0)
+video_capture_object = cv2.VideoCapture(0)
 
 while True:
     # capture frame by frame
-    ret, frame = cap.read()
+    ret, frame = video_capture_object.read()
 
     # frame operations
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-cap.release()
+video_capture_object.release()
 cv2.destroyAllWindows()
